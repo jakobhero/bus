795af73a6f4ebb007b06dbb149219ff619f21cf9
MohamedEl1 <mohamedgenak@gmail.com>
2020-06-26 14:42:13 +0100

    show map, search autocomplete, fixes

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { MoviesData, renderMovieTitle } from './stopss.js';
+import Autocomplete from 'react-autocomplete';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = { val: '' };
+  
+
+  render() {
+    return (
+      <div className="autocomplete-wrapper">
+        <h3>Search for a bus stop number</h3>
+        <Autocomplete
+          value={this.state.val}
+          items={MoviesData()}
+          getItemValue={item => item.id}
+          shouldItemRender={renderMovieTitle}
+          renderMenu={item => (
+            <div className="dropdown">
+              {item}
+            </div>
+          )}
+          renderItem={(item, isHighlighted) =>
+            <div className={`item ${isHighlighted ? 'selected-item' : ''}`}>
+              {item.id}
+            </div>
+          }
+          onChange={(event, val) => this.setState({ val })}
+          onSelect={val => this.setState({ val })}
+        />
+      </div>
+      
+    );
+  }
 }
 
 export default App;
