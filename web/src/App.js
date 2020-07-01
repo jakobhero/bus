a11c0be07dbe91ec76d08ab950a8cd4406e4ac62
CallumT45 <callumterr45@gmail.com>
2020-07-01 12:25:27 +0100

    Adjusted form and started mapping comp

--- web/src/App.js
+++ web/src/App.js
@@ -1,38 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { Component } from 'react';
-import { MoviesData, renderMovieTitle } from './stopss.js';
-import Autocomplete from 'react-autocomplete';
+import "./App.css";
+import React, { Component } from "react";
+import SearchLocationInput from "./SearchLocationInput";
 
 class App extends Component {
-
-  state = { val: '' };
-  
+  state = { val: "" };
 
   render() {
     return (
       <div className="autocomplete-wrapper">
         <h3>Search for a bus stop number</h3>
-        <Autocomplete
-          value={this.state.val}
-          items={MoviesData()}
-          getItemValue={item => item.id}
-          shouldItemRender={renderMovieTitle}
-          renderMenu={item => (
-            <div className="dropdown">
-              {item}
-            </div>
-          )}
-          renderItem={(item, isHighlighted) =>
-            <div className={`item ${isHighlighted ? 'selected-item' : ''}`}>
-              {item.id}
-            </div>
-          }
-          onChange={(event, val) => this.setState({ val })}
-          onSelect={val => this.setState({ val })}
-        />
+        <SearchLocationInput></SearchLocationInput>
       </div>
-      
     );
   }
 }
--- web/src/index.js
+++ web/src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import ShowMap from './components/ShowMap';
-import SideMenu from './components/SideMenu';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import ShowMap from "./components/ShowMap";
+// import SideMenu from './components/SideMenu';
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-
+    {/* <ShowMap></ShowMap> */}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
