bc4d823380567d56610272dfb5215799a2c65b0b
CallumT45 <callumterry45@gmail.com>
2020-07-30 23:27:15 +0100

    Test new connections, other fixes

--- web/src/components/allRoutes.js
+++ web/src/components/allRoutes.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Route from "./route";
-
+import { Card } from "antd";
 const AllRoutes = ({ tripTimes, setDirections }) => {
   return (
     <div>
-      {tripTimes.length > 0 &&
-        tripTimes.map((dueTime, i) => (
-          <Route key={i} tripTime={dueTime} setDirections={setDirections} />
-        ))}
-      {tripTimes.length < 1 && <p>Choose a source and destination</p>}
+      <Card headStyle={{ backgroundColor: "#1b55db" }}>
+        {tripTimes.length > 0 &&
+          tripTimes.map((dueTime, i) => (
+            <Route key={i} tripTime={dueTime} setDirections={setDirections} />
+          ))}
+        {tripTimes.length < 1 && <p>Choose a source and destination</p>}
+      </Card>
     </div>
   );
 };
