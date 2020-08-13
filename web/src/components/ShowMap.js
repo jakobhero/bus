670310675726ef2780324b01dc3a79ff5dd40d75
CallumT45 <callumterry45@gmail.com>
2020-08-13 18:07:41 +0100

    Fixed bug with compass ofter other route switch

--- web/src/components/ShowMap.js
+++ web/src/components/ShowMap.js
@@ -41,7 +41,7 @@ const centre = {
   lat: 53.35014,
   lng: -6.266155,
 };
-function Locate({ panTo, getStopsByCoords }) {
+function Locate({ panTo, getStopsByCoords, setOherRouteBool }) {
   return (
     <Tooltip className="tooltip" title="Current Location">
       <div
@@ -58,6 +58,7 @@ function Locate({ panTo, getStopsByCoords }) {
                 position.coords.longitude,
                 true
               );
+              setOherRouteBool(false);
             },
             () => null,
             {
@@ -213,7 +214,11 @@ function ShowMap({
         onLoad={onMapLoad}
         onClick={onMapClick}
       >
-        <Locate panTo={panTo} getStopsByCoords={getStopsByCoords} />
+        <Locate
+          panTo={panTo}
+          getStopsByCoords={getStopsByCoords}
+          setOherRouteBool={setOherRouteBool}
+        />
         <Tooltip className="tooltip" title="Tourist mode">
           <div className="switch1 mapUI">
             <Switch
