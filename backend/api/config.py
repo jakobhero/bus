276506ae5662dc13f3784344ade66e5b070616ba
Jakob Herold <jakob.herold.jh@gmail.com>
2020-08-25 13:51:42 +0100

    keys hidden

--- backend/api/config.py
+++ backend/api/config.py
@@ -5,5 +5,5 @@ class Config(object):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:super_pw@localhost:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    GOOGLE_KEY = "AIzaSyB2pRGqvZyPk9HhYUvpShe9KUHFASAq9kI"
-    OWM_KEY = "0324f68680731d1f1c47d73aef71e333"
+    GOOGLE_KEY = "YOUR_GOOGLE_KEY"
+    OWM_KEY = "YOUR_OWM_KEY"
